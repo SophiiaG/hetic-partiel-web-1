7747bcf57a38cc392237fd3c581e2df521ceaefa
SophiiaG <sophiagounani@gmail.com>
2018-06-14 12:41:13 +0200

    Menu n'apparaît pas correctement sur mobile #1 (pas finit)

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
+// Scrool to top
+
 var timeOut;
 
-function scrollToTop(){
-  if(document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
-    window.scrollBy(0,-50);
-    timeOut=setTimeout('scrollToTop()', 10);
-  }
-  else clearTimeout(timeOut)
+function scrollToTop() {
+  if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
+    window.scrollBy(0, -50);
+    timeOut = setTimeout('scrollToTop()', 10);
+  } else clearTimeout(timeOut)
 }
 
 var nav = document.querySelector('.top');
 
-nav.addEventListener('click', function(){
-scrollToTop()
+nav.addEventListener('click', function() {
+  scrollToTop()
 });
 
 
+// Navigation
 
 
 var back = document.querySelector('.fixed');
@@ -22,8 +24,8 @@ var position = 0;
 
 // scrollY renvoie le nombre de pixels défilé verticalement
 
-window.addEventListener('scroll', function(){
-  if(position < window.scrollY) {
+window.addEventListener('scroll', function() {
+  if (position < window.scrollY) {
     back.classList += 'up';
     // on ajoute la class up afin de caché la nav
     position = window.scrollY;
@@ -35,3 +37,23 @@ window.addEventListener('scroll', function(){
     // on reset la variable position pour lui donné la valeur exacte d'où elle se situe
   }
 })
+
+
+// Burger menu
+
+
+var burgermenu = document.querySelector('.burgermenu');
+var menu = document.querySelector('#topbar');
+counter = 0
+
+burgermenu.addEventListener('click', function() {
+  counter++
+  console.log(counter);
+  if (counter === 1) {
+    menu.style.display = 'flex';
+  }
+  if (counter === 2) {
+    menu.style.display = 'none';
+    counter = 0
+  }
+});
