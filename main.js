4fd8ba86f8d076a18c03af3ffeb33ca94766e1bc
SophiiaG <sophiagounani@gmail.com>
2018-06-14 12:34:52 +0200

    Navigation non visible quand on scroll #2 correction

--- main.js
+++ main.js
@@ -16,19 +16,20 @@ scrollToTop()
 
 
 
-var nav = document.querySelector('.fixed');
+
+var back = document.querySelector('.fixed');
 var position = 0;
 
 // scrollY renvoie le nombre de pixels défilé verticalement
 
 window.addEventListener('scroll', function(){
   if(position < window.scrollY) {
-    nav.classList += 'up';
+    back.classList += 'up';
     // on ajoute la class up afin de caché la nav
     position = window.scrollY;
     // on reset la variable position pour lui donné la valeur exacte d'où elle se situe
   } else {
-    nav.classList = 'fixed small'
+    back.classList = 'fixed small'
     // on ajoute la class small afin de faire apparaitre la nav
     position = window.scrollY;
     // on reset la variable position pour lui donné la valeur exacte d'où elle se situe
